1752b99bf080d085d44055e8c34216186220a473
TotallyLuke <lucaveronese99@gmail.com>
2022-05-14 14:07:53 +0200

    Close #1, update `ComputeObjective`

--- src/main/java/G050HW2.java
+++ src/main/java/G050HW2.java
@@ -9,35 +9,7 @@ import java.util.*;
 
 public class G050HW2 {
 
-
-
     private static double[][] distanceMatrix;
-
-// &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
-// Input reading methods
-// &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
-    public static Vector strToVector(String str) {
-        String[] tokens = str.split(",");
-        double[] data = new double[tokens.length];
-        for (int i = 0; i < tokens.length; ++i) {
-            data[i] = Double.parseDouble(tokens[i]);
-        }
-        return Vectors.dense(data);
-    }
-
-    public static ArrayList<Vector> readVectorsSeq(String filename) throws IOException {
-        if (Files.isDirectory(Paths.get(filename))) {
-            throw new IllegalArgumentException("readVectorsSeq is meant to read a single file.");
-        }
-
-        ArrayList<Vector> result = new ArrayList<>();
-        Files.lines(Paths.get(filename))
-                .map(str -> strToVector(str))
-                .forEach(e -> result.add(e));
-
-        return result;
-    }
-
 
     /**
      * Implements the weighted variant of kcenterOUT
@@ -81,14 +53,15 @@ public class G050HW2 {
             }
 
             while (S.size() < k && Wz > 0) {
-                // PREcondition: Z must be greater than 0
+                // PREcondition: Z must be greater than 0 (true because Wz > 0)
                 //TODO: why here max is not initialized as 0 (as in the pseudocode)? In this way you are also checking twice the first point: here and in the for loop
+                //@ftrole newcenter if not initialized then error in line 71, either create an empty Vector or newcenter = P.get(first)
                 int first = Z_indexes.get(0);
                 Vector newcenter = P.get(first);        // dummy inizialization to avoid error
-                double max = getBallWeight(r, alpha, Z_indexes, first, W);   // max ball weight
+                double max = getBallWeight(first, r, alpha, Z_indexes, W);   // max ball weight
                 int i_max = first;
-                for (int i = 0; i < P.size(); ++i) {    // find max ball, assign center and value of it to newcenter and max
-                    double ball_weight = getBallWeight(r,  alpha, Z_indexes, i, W);
+                for (int i = 1; i < P.size(); ++i) {    // find max ball, assign center and value of it to newcenter and max
+                    double ball_weight = getBallWeight(i, r,  alpha, Z_indexes, W);
                     if (ball_weight > max) {
                         max = ball_weight;
                         newcenter = P.get(i);
@@ -121,13 +94,14 @@ public class G050HW2 {
     /**
      * Computes the weight of ball centered in c_i of radius (1 + 2*alpha) * r
      *
-     * @param r radius of ball
-     * @param alpha alpha
+     * @param i         index of ongoing Point c_i
+     * @param r         radius of ball
+     * @param alpha     alpha
      * @param Z_indexes indexes in Uncovered set
-     * @param i index of ongoing Point c_i
+     * @param W         weights
      * @return ball weight
      */
-    private static double getBallWeight(double r, double alpha, List<Integer> Z_indexes, int i, ArrayList<Long> W) {
+    private static double getBallWeight(int i, double r, double alpha, List<Integer> Z_indexes, ArrayList<Long> W) {
         double ball_weight = 0.0;
         for (int j : Z_indexes) {
             if (distanceMatrix[i][j] <= (1 + 2*alpha) * r) {
@@ -136,7 +110,6 @@ public class G050HW2 {
         }
         return ball_weight;
     }
-
 
     /**
      * Computes the value of the objective function
@@ -147,21 +120,25 @@ public class G050HW2 {
      * @return value of the objective function
      */
     public static double ComputeObjective(ArrayList<Vector> P, ArrayList<Vector> S, int z) {
-        ArrayList<Double> distsToCenters = new ArrayList<>(P.size());
+        //no problem if points are overlapping
+        ArrayList<Double> distsToCenters = new ArrayList<>(Collections.nCopies(P.size(), Double.MAX_VALUE));
 
-        for (Vector p : P) {
-            double nearNeighP = Double.MAX_VALUE;
-            for (Vector s : S) {
-                final double cand = Math.sqrt(Vectors.sqdist(p, s));
-                if (cand < nearNeighP) {
-                    nearNeighP = cand;
+        for(Vector s : S) {
+            final int indS = P.indexOf(s); // compute indexes (in P) of elements in S
+
+            // to keep update distsToCenters with distances to subset of visited centers
+            for (int p = 0; p < P.size(); ++p) {
+                if (distanceMatrix[indS][p] < distsToCenters.get(p)) {
+                    distsToCenters.set(p, distanceMatrix[indS][p]);
                 }
             }
-            distsToCenters.add(nearNeighP);
         }
 
-
         distsToCenters.sort(Comparator.naturalOrder());
+
+        if(distsToCenters.get(S.size() - 1) != 0)   // to demonstrate that first s elements are 0
+            throw new AssertionError("Every center must be distant 0 from centers");
+
         return distsToCenters.get(P.size() - 1 - z);
     }
 
@@ -178,11 +155,6 @@ public class G050HW2 {
         ArrayList<Vector> inputPoints = readVectorsSeq(path);
         ArrayList<Long> weights = new ArrayList<>(Collections.nCopies(inputPoints.size(), 1L));
 
-        //TODO: how can this happen since weights is initialized to have the same length of inputPoints?
-        if (weights.size() != inputPoints.size() || weights.size() == 0)
-            throw new AssertionError("ERROR: len(W) != len(P)");
-
-
         System.out.println("Input size n = " + inputPoints.size());
         System.out.println("Number of centers k = " + k);
         System.out.println("Number of outliers z = " + z);
@@ -190,7 +162,6 @@ public class G050HW2 {
         final long startTime = System.currentTimeMillis();
 
         // Distance Matrix calculation
-        // maybe is better an arraylist of arraylists
         distanceMatrix = new double[inputPoints.size()][inputPoints.size()];
         for (int i = 0; i < inputPoints.size(); ++i) {
             for (int j = i + 1; j < inputPoints.size(); ++j) {
@@ -201,10 +172,35 @@ public class G050HW2 {
         ArrayList<Vector> solution = SeqWeightedOutliers(inputPoints, weights, k, z, 0);
         final long timeElapsed = System.currentTimeMillis() - startTime;
 
-        double objective = ComputeObjective(inputPoints, solution, z);
-
+        final double objective = ComputeObjective(inputPoints, solution, z);
 
         System.out.println("Objective function = " + objective);
         System.out.println("Time of SeqWeightedOutliers = " + timeElapsed);
+    }
+
+    // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+    // Input reading methods
+    // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+    public static Vector strToVector(String str) {
+        String[] tokens = str.split(",");
+        double[] data = new double[tokens.length];
+        for (int i = 0; i < tokens.length; ++i) {
+            data[i] = Double.parseDouble(tokens[i]);
+        }
+
+        return Vectors.dense(data);
+    }
+
+    public static ArrayList<Vector> readVectorsSeq(String filename) throws IOException {
+        if (Files.isDirectory(Paths.get(filename))) {
+            throw new IllegalArgumentException("readVectorsSeq is meant to read a single file.");
+        }
+
+        ArrayList<Vector> result = new ArrayList<>();
+        Files.lines(Paths.get(filename))
+                .map(str -> strToVector(str))
+                .forEach(e -> result.add(e));
+
+        return result;
     }
 }
